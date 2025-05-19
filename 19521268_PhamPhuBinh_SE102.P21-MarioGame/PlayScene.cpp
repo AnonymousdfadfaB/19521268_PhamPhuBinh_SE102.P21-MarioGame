6b8dfa7f7c23122cc9350a534ecd09a3bc067624
AnonymousdfadfaB <19521268@gm.uit.edu.vn>
2025-05-19 22:59:13 +0700

    red goomba almost done

--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/PlayScene.cpp
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/PlayScene.cpp
@@ -125,6 +125,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	}
 
 	case OBJECT_TYPE_BROWN_GOOMBA: obj = new CBrownGoomba(x,y); break;
+	case OBJECT_TYPE_RED_GOOMBA: obj = new CRedGoomba(x, y); break;
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 
--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/PlayScene.h
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/PlayScene.h
@@ -6,6 +6,7 @@
 #include "Brick.h"
 #include "Mario.h"
 #include "BrownGoomba.h"
+#include "RedGoomba.h"
 //#include "Koopas.h"
 
 
