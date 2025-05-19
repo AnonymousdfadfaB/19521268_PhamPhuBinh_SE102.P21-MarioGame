a4eebb002419bce8c1cf670e76ac749eb1fdae42
AnonymousdfadfaB <19521268@gm.uit.edu.vn>
2025-05-19 12:04:42 +0700

    try to implement Red Goomba

--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/Goomba.cpp
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/Goomba.cpp
@@ -1,11 +1,11 @@
 #include "Goomba.h"
 
-CGoomba::CGoomba(float x, float y):CGameObject(x, y)
+CGoomba::CGoomba(float x, float y, int type):CGameObject(x, y)
 {
-	this->ax = 0;
-	this->ay = GOOMBA_GRAVITY;
+
+	this->type = type;
 	die_start = -1;
-	SetState(GOOMBA_STATE_WALKING);
+	SetState(GOOMBA_STATE_WALKING_LEFT);
 }
 
 void CGoomba::GetBoundingBox(float &left, float &top, float &right, float &bottom)
@@ -35,7 +35,6 @@ void CGoomba::OnNoCollision(DWORD dt)
 void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
 {
 	if (!e->obj->IsBlocking()) return; 
-	if (dynamic_cast<CGoomba*>(e->obj)) return; 
 
 	if (e->ny != 0 )
 	{
@@ -43,14 +42,32 @@ void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
 	}
 	else if (e->nx != 0)
 	{
-		vx = -vx;
+		if (state == GOOMBA_STATE_WALKING_LEFT)
+		{
+			SetState(GOOMBA_STATE_WALKING_RIGHT);
+		}
+		else if (state == GOOMBA_STATE_WALKING_RIGHT)
+		{
+			SetState(GOOMBA_STATE_WALKING_LEFT);
+		} 
+		if (type == GOOMBA_TYPE_RED)
+		{
+			if (state == GOOMBA_STATE_WALKING_JUMP_LEFT)
+			{
+				SetState(GOOMBA_STATE_WALKING_JUMP_RIGHT);
+			}
+			else if (state == GOOMBA_STATE_WALKING_JUMP_RIGHT)
+			{
+				SetState(GOOMBA_STATE_WALKING_JUMP_LEFT);
+			}
+		}
 	}
 }
 
 void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 {
-	vy += ay * dt;
-	vx += ax * dt;
+	vy += GOOMBA_GRAVITY * dt;
+
 
 	if ( (state==GOOMBA_STATE_DIE) && (GetTickCount64() - die_start > GOOMBA_DIE_TIMEOUT) )
 	{
@@ -66,17 +83,29 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 void CGoomba::Render()
 {
 	int aniId = ID_ANI_GOOMBA_WALKING;
-	if (state == GOOMBA_STATE_DIE) 
-	{
-		aniId = ID_ANI_GOOMBA_DIE;
-	}
-
+	if (type == GOOMBA_TYPE_RED)
+		aniID = GetAniIdGoombaRed();
+	else
+		aniID = GetAniIdGoombaBrown();
 	CAnimations::GetInstance()->Get(aniId)->Render(x,y);
 }
 
 void CGoomba::SetState(int state)
 {
-	CGameObject::SetState(state);
+	if (type == GOOMBA_TYPE_RED)
+	{
+		switch (state)
+		{
+		case GOOMBA_STATE_WALKING_JUMP_LEFT:
+			vx = -GOOMBA_WALKING_JUMP_SPEED;
+			vy = -GOOMBA_WALKING_JUMP_SPEED;
+			break;
+		case GOOMBA_STATE_WALKING_JUMP_RIGHT:
+			vx = GOOMBA_WALKING_JUMP_SPEED;
+			vy = -GOOMBA_WALKING_JUMP_SPEED;
+			break;
+		}
+	}
 	switch (state)
 	{
 		case GOOMBA_STATE_DIE:
@@ -84,10 +113,32 @@ void CGoomba::SetState(int state)
 			y += (GOOMBA_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE)/2;
 			vx = 0;
 			vy = 0;
-			ay = 0; 
 			break;
-		case GOOMBA_STATE_WALKING: 
+		case GOOMBA_STATE_WALKING_LEFT: 
 			vx = -GOOMBA_WALKING_SPEED;
 			break;
+		case GOOMBA_STATE_WALKING_RIGHT:
+			vx = GOOMBA_WALKING_SPEED;
+			break;
 	}
+
+	CGameObject::SetState(state);
+}
+int CGoomba::GetAniIdGoombaBrown()
+{
+	if (state == GOOMBA_STATE_DIE)
+		return ID_ANI_GOOMBA_BROWN_DIE;
+	else if (state == GOOMBA_STATE_WALKING_LEFT || state == GOOMBA_STATE_WALKING_RIGHT)
+		return ID_ANI_GOOMBA_BROWN_WALKING;
+}
+int CGoomba::GetAniIdGoombaRed()
+{
+	if (state == GOOMBA_STATE_DIE)
+		return ID_ANI_GOOMBA_RED_DIE;
+	else if (state == GOOMBA_STATE_WALKING_LEFT || state == GOOMBA_STATE_WALKING_RIGHT)
+		return ID_ANI_GOOMBA_RED_WALKING;
+	else if (state == GOOMBA_STATE_WALKING_JUMP_LEFT || state == GOOMBA_STATE_WALKING_JUMP_RIGHT)
+		return ID_ANI_GOOMBA_WALKING_JUMP_LEFT;
+	else if (state == GOOMBA_STATE_WALKING_JUMP_RIGHT)
+		return ID_ANI_GOOMBA_WALKING_JUMP_RIGHT;
 }
--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/Goomba.h
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/Goomba.h
@@ -11,18 +11,29 @@
 
 #define GOOMBA_DIE_TIMEOUT 500
 
-#define GOOMBA_STATE_WALKING 100
-#define GOOMBA_STATE_DIE 200
 
-#define ID_ANI_GOOMBA_WALKING 5000
-#define ID_ANI_GOOMBA_DIE 5001
+
+#define ID_ANI_GOOMBA_BROWN_WALKING 5000
+#define ID_ANI_GOOMBA_BROWN_DIE 5001
+
+#define ID_ANI_GOOMBA_RED_WALKING 5002
+#define ID_ANI_GOOMBA_RED_DIE 5003
+#define ID_ANI_GOOMBA_BROWN_WALKING_JUMP 5004
+
+#define GOOMBA_TYPE_RED 1
+#define GOOMBA_TYPE_BROWN 2
+
+#define GOOMBA_STATE_WALKING_LEFT 100
+#define GOOMBA_STATE_WALKING_RIGHT 101
+#define GOOMBA_STATE_WALKING_JUMP_LEFT 102
+#define GOOMBA_STATE_WALKING_JUMP_RIGHT 103
+#define GOOMBA_STATE_DIE 104
 
 class CGoomba : public CGameObject
 {
 protected:
-	float ax;				
-	float ay; 
-
+	int type;
+	int state;
 	ULONGLONG die_start;
 
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
@@ -36,6 +47,8 @@ protected:
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 
 public: 	
-	CGoomba(float x, float y);
+	CGoomba(float x, float y, int type);
 	virtual void SetState(int state);
+	int GetAniIdGoombaRed();
+	int GetAniIdGoombaBrown();
 };
