a2e9e54d0029d57846976b2ca9c07705d9cc10ce
AnonymousdfadfaB <19521268@gm.uit.edu.vn>
2025-05-21 21:09:10 +0700

    nearly done plant enemy

--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/NoFirePlant.cpp
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/NoFirePlant.cpp
@@ -1,33 +1,54 @@
 
 #include "NoFirePlant.h"
 
-CNoFirePlant::CNoFirePlant(float x, float y, CGameObject* pipe):CGameObject(x, y)
+CNoFirePlant::CNoFirePlant(float x, float y, int type, CGameObject* pipe):CGameObject(x, y)
 {
-	minY = y;
-	maxY = y - NO_FIRE_GREEN_PLANT_HEIGHT;
+	this->type = type;
+	switch (type)
+	{
+	case NO_FIRE_GREEN_PLANT:
+		minY = y;
+		maxY = y - NO_FIRE_GREEN_PLANT_HEIGHT;
+		canFire = false;
+		break;
+	case FIRE_GREEN_PLANT:
+		minY = y;
+		maxY = y - FIRE_GREEN_PLANT_HEIGHT;
+		canFire = true;
+		break;
+	case FIRE_RED_PLANT:
+		minY = y;
+		maxY = y - FIRE_RED_PLANT_HEIGHT;
+		canFire = true;
+		break;
+	}
+	hide_start = -1;
+	attack_start = -1;
+	alreadyFire = false;
 	this->pipe = pipe;
-	SetState(NO_FIRE_GREEN_PLANT_STATE_HIDE);
+	SetState(HIDE_STATE);
 }
 void CNoFirePlant::SetState(int state)
 {
 
 	switch (state)
 	{
-	case NO_FIRE_GREEN_PLANT_STATE_HIDE:
+	case HIDE_STATE:
 		vy = 0;
 		hide_start = GetTickCount64();
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_RISE:
+	case RISE_STATE:
 		vy = -NO_FIRE_GREEN_PLANT_VERTICAL_SPEED;
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_ATTACK:
+	case ATTACK_STATE:
 		attack_start = GetTickCount64();
+		alreadyFire = false;
 		vy = 0;
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_DOWN:
+	case DOWN_STATE:
 		vy = NO_FIRE_GREEN_PLANT_VERTICAL_SPEED;
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_DIE:
+	case DIE_STATE:
 		vy = 0;
 		break;
 	}
@@ -35,41 +56,47 @@ void CNoFirePlant::SetState(int state)
 }
 void CNoFirePlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	if (state == NO_FIRE_GREEN_PLANT_STATE_DIE)
+	if (state == DIE_STATE)
 	{
 		isDeleted = true;
 		return;
 	}
 	switch (state)
 	{
-	case NO_FIRE_GREEN_PLANT_STATE_HIDE:
+	case HIDE_STATE:
 		if (GetTickCount64() - hide_start > HIDE_DURATION)
 		{
 			hide_start = -1;
-			SetState(NO_FIRE_GREEN_PLANT_STATE_RISE);
+			SetState(RISE_STATE);
 		}
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_RISE:
+	case RISE_STATE:
 		if (y <= maxY)
 		{
 			y = maxY;
-			SetState(NO_FIRE_GREEN_PLANT_STATE_ATTACK);
+			SetState(ATTACK_STATE);
 		}
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_ATTACK:
+	case ATTACK_STATE:
 		if (GetTickCount64() - attack_start > ATTACK_DURATION)
 		{
 			attack_start = -1;
-			SetState(NO_FIRE_GREEN_PLANT_STATE_DOWN);
+			SetState(DOWN_STATE);
 		}
 		break;
-	case NO_FIRE_GREEN_PLANT_STATE_DOWN:
+	case DOWN_STATE:
 		if (y >= minY)
 		{
 			y = minY;
-			SetState(NO_FIRE_GREEN_PLANT_STATE_HIDE);
+			SetState(HIDE_STATE);
 		}
 		break;
+	}
+
+	if (canFire && state == ATTACK_STATE && !alreadyFire)
+	{
+		((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->AddObject(new CMario(x, y)); //CFireBall(x, y)
+		alreadyFire = true;
 	}
 	y += vy * dt;
 	//CCollision::GetInstance()->Process(this, dt, coObjects);
@@ -86,16 +113,89 @@ void CNoFirePlant::OnCollisionWith(LPCOLLISIONEVENT e)
 }
 void CNoFirePlant::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
-	left = x - NO_FIRE_GREEN_PLANT_WIDTH / 2;
-	top = y - NO_FIRE_GREEN_PLANT_HEIGHT / 2;
-	right = left + NO_FIRE_GREEN_PLANT_WIDTH;
-	bottom = top + NO_FIRE_GREEN_PLANT_HEIGHT;
+	switch(type)
+	{
+	case NO_FIRE_GREEN_PLANT:
+		left = x - NO_FIRE_GREEN_PLANT_WIDTH / 2;
+		top = y - NO_FIRE_GREEN_PLANT_HEIGHT / 2;
+		right = left + NO_FIRE_GREEN_PLANT_WIDTH;
+		bottom = top + NO_FIRE_GREEN_PLANT_HEIGHT;
+		break;
+	case FIRE_GREEN_PLANT:
+		left = x - FIRE_GREEN_PLANT_WIDTH / 2;
+		top = y - FIRE_GREEN_PLANT_HEIGHT / 2;
+		right = left + FIRE_GREEN_PLANT_WIDTH;
+		bottom = top + FIRE_GREEN_PLANT_HEIGHT;
+		break;
+	case FIRE_RED_PLANT:
+		left = x - FIRE_RED_PLANT_WIDTH / 2;
+		top = y - FIRE_RED_PLANT_HEIGHT / 2;
+		right = left + FIRE_RED_PLANT_WIDTH;
+		bottom = top + FIRE_RED_PLANT_HEIGHT;
+		break;
+	}
+
 }
 
 void CNoFirePlant::Render()
 {
-	int aniId = NO_FIRE_GREEN_PLANT_ANI_ID;
+	int aniId;
+	switch (type)
+	{
+	case NO_FIRE_GREEN_PLANT:
+		aniId = NO_FIRE_GREEN_PLANT_ANI_ID;
+		break;
+	case FIRE_GREEN_PLANT:
+		switch (GetMarioRelativePosition())
+		{
+		case MARIO_POSITION_LEFT_TOP:
+			aniId = FIRE_GREEN_PLANT_LEFT_TOP_ANI_ID;
+			break;
+		case MARIO_POSITION_RIGHT_TOP:
+			aniId = FIRE_GREEN_PLANT_RIGHT_TOP_ANI_ID;
+			break;
+		case MARIO_POSITION_LEFT_BOTTOM:
+			aniId = FIRE_GREEN_PLANT_LEFT_BOTTOM_ANI_ID;
+			break;
+		case MARIO_POSITION_RIGHT_BOTTOM:
+			aniId = FIRE_GREEN_PLANT_RIGHT_BOTTOM_ANI_ID;
+			break;
+		}
+		break;
+		/*
+	case FIRE_RED_PLANT:
+		switch (GetMarioRelativePosition())
+		{
+		case MARIO_POSITION_LEFT_TOP:
+			aniId = FIRE_RED_PLANT_LEFT_TOP_ANI_ID;
+			break;
+		case MARIO_POSITION_RIGHT_TOP:
+			aniId = FIRE_RED_PLANT_RIGHT_TOP_ANI_ID;
+			break;
+		case MARIO_POSITION_LEFT_BOTTOM:
+			aniId = FIRE_RED_PLANT_LEFT_BOTTOM_ANI_ID;
+			break;
+		case MARIO_POSITION_RIGHT_BOTTOM:
+			aniId = FIRE_RED_PLANT_RIGHT_BOTTOM_ANI_ID;
+			break;
+		}
+		break;
+		*/
+	}
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+}
+int CNoFirePlant::GetMarioRelativePosition()
+{
+	int position = 0;
+	CMario* mario = (CMario*)((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+
+	if (mario->GetY() > y)
+		position += 2;
+	if (mario->GetX() < x)
+		position += 1;
+	else
+		position += 2;
+	return position;
 }
 
 
--- 19521268_PhamPhuBinh_SE102.P21-MarioGame/NoFirePlant.h
+++ 19521268_PhamPhuBinh_SE102.P21-MarioGame/NoFirePlant.h
@@ -1,24 +1,45 @@
 #pragma once
 #include "GameObject.h"
-//#include "Pipe.h"
-
+#include "Game.h"
+#include "Scene.h"
+#include "PlayScene.h"
+//#include "Pipe.h" due to loop of include dependency
+//operation parameters 
 #define HIDE_DURATION 2000.0f
 #define ATTACK_DURATION 2000.0f
 #define NO_FIRE_GREEN_PLANT_VERTICAL_SPEED 0.05f
-
+//width and height
 #define NO_FIRE_GREEN_PLANT_WIDTH 16
 #define NO_FIRE_GREEN_PLANT_HEIGHT 24
 
-#define NO_FIRE_GREEN_PLANT_STATE_HIDE 100
-#define NO_FIRE_GREEN_PLANT_STATE_RISE 101
-#define NO_FIRE_GREEN_PLANT_STATE_ATTACK 102
-#define NO_FIRE_GREEN_PLANT_STATE_DOWN 103
-#define NO_FIRE_GREEN_PLANT_STATE_DIE 104
+#define FIRE_GREEN_PLANT_WIDTH 16
+#define FIRE_GREEN_PLANT_HEIGHT 24
 
+#define FIRE_RED_PLANT_WIDTH 16
+#define FIRE_RED_PLANT_HEIGHT 32
+//states
+#define	HIDE_STATE 100
+#define RISE_STATE 101
+#define ATTACK_STATE 102
+#define DOWN_STATE 103
+#define DIE_STATE 104
+//animation ids
 #define NO_FIRE_GREEN_PLANT_ANI_ID 84001
-#define NO_FIRE_PLANT_TYPE_GREEN 1
-#define NO_FIRE_PLANT_TYPE_RED 2
 
+#define FIRE_GREEN_PLANT_LEFT_TOP_ANI_ID 85001
+#define FIRE_GREEN_PLANT_RIGHT_TOP_ANI_ID 85002
+#define FIRE_GREEN_PLANT_LEFT_BOTTOM_ANI_ID 85003
+#define FIRE_GREEN_PLANT_RIGHT_BOTTOM_ANI_ID 85004
+
+//type
+#define NO_FIRE_GREEN_PLANT 1
+#define FIRE_GREEN_PLANT 2
+#define FIRE_RED_PLANT	3
+//mario position
+#define MARIO_POSITION_LEFT_TOP 1
+#define MARIO_POSITION_RIGHT_TOP 2
+#define MARIO_POSITION_LEFT_BOTTOM 3
+#define MARIO_POSITION_RIGHT_BOTTOM 4
 class CNoFirePlant : public CGameObject
 {
 protected:
@@ -26,6 +47,8 @@ protected:
 	int type;
 	float maxY;
 	float minY;
+	bool canFire;
+	bool alreadyFire;
 	ULONGLONG hide_start;
 	ULONGLONG attack_start;
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
@@ -37,8 +60,9 @@ protected:
 	virtual void OnNoCollision(DWORD dt);
 
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	int GetMarioRelativePosition();
 
 public:
-	CNoFirePlant(float x, float y, CGameObject* pipe);
+	CNoFirePlant(float x, float y, int type, CGameObject* pipe);
 	virtual void SetState(int state);
 };
